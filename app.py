b04835d9bdaa6e57b427bb5ae6175dd2ecd470aa
N-Harish <harishnatarajan24@gmail.com>
2021-02-10 18:26:22 +0530

    LDA Viz

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask import  render_template_string
+
+
+app = Flask(__name__)
+
+with open('lda_vis.html', 'r') as f:
+    li = str(f.read())
+
+
+@app.route('/')
+def home():
+    return render_template_string(li)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
